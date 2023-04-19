9cbaf618e1d1e4e0142b28dcb4e0cf93cb9fbac7
Balaji Jethliya <74290492+jethliya-balaji@users.noreply.github.com>
2023-04-19 21:45:02 +0530

    Update models.py
    added default=None

--- src/chat/models.py
+++ src/chat/models.py
@@ -10,7 +10,7 @@ class Room(models.Model):
     id           = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False) 
     name         = models.CharField(max_length=50, unique=True)
     private_room = models.BooleanField(default=False)
-    passcode     = models.CharField(max_length=128, blank=True, null=True)
+    passcode     = models.CharField(max_length=128, blank=True, null=True, default=None)
     created_by   = models.ForeignKey(User, related_name='created_rooms', on_delete=models.CASCADE)
     created_at   = models.DateTimeField(auto_now_add=True)
 
